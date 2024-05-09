031a44a6bf576f1111882f68305373bea0e87c7d
shigcodes <shigridanndocil11@gmail.com>
2024-05-09 10:15:56 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-4xl font-bold">Hello, I'm CursorBot</h1>
+        </div>
+    );
+};
+
+export default Hero;
+
