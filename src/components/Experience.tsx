a4c57d183ea05e08bf7c5babe567afb8989a99ea
shigcodes <shigridanndocil11@gmail.com>
2024-05-09 16:33:28 +0800

    Initialize progress

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,10 +13,28 @@ const Experience = () => {
             <p>
                 Explore my work experience and let's discover the potential for collaboration.
             </p>
-            <div className="flex flex-col items-center justify-between border border-black">
-                <h1 className="text-2xl"> New York, United States</h1>
-                <p>September 2023 – April 2024</p>
+            <div className="grid grid-cols-12 border-b border-black flex justify-between flex-row" style={{ width: '50%' }}>
+                <div className="flex flex-col items-center justify-between border border-black col-span-6 p-4">
+                    <h1 className="text-2xl"> New York, United States</h1>
+                    <p className="text-sm text-gray-400">September 2023 – April 2024</p>
+                    <h1 className="text-2xl"> Hamburg, Germany</h1>
+                    <p className="text-sm text-gray-400">April 2023 – November 2024</p>
+                    <h1 className="text-2xl"> Manila, Philippines</h1>
+                    <p className="text-sm text-gray-400">May 2020 – July 2021</p>
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center' }} className="col-span-6 border border-black" >
+                    <img src="exp.png" alt="exp" />
+                    <div className="flex flex-col items-center justify-between border border-black col-span-6 p-4">
+                        <h1 className="text-2xl"> iOS Frontend Developer</h1>
+                        <p className="text-sm text-gray-400">Lickability</p>
+                        <h1 className="text-2xl"> Event Specialist</h1>
+                        <p className="text-sm text-gray-400">Stackstream Dev Community</p>
+                        <h1 className="text-2xl"> Freelance Graphic Designer</h1>
+                        <p className="text-sm text-gray-400">PHB Worx International Inc.</p>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
